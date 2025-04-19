e2674b0e29f790a07c66a3904a3785d2036478a8
Vasileios Zois <vazois@microsoft.com>
2025-04-19 12:46:52 -0700

    make migrate command execute in the background

--- libs/cluster/Session/RespClusterMigrateCommands.cs
+++ libs/cluster/Session/RespClusterMigrateCommands.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using Garnet.common;
 using Garnet.server;
 using Microsoft.Extensions.Logging;
@@ -11,6 +12,13 @@ using Tsavorite.core;
 
 namespace Garnet.cluster
 {
+    using BasicGarnetApi = GarnetApi<BasicContext<SpanByte, SpanByte, RawStringInput, SpanByteAndMemory, long, MainSessionFunctions,
+        /* MainStoreFunctions */ StoreFunctions<SpanByte, SpanByte, SpanByteComparer, SpanByteRecordDisposer>,
+        SpanByteAllocator<StoreFunctions<SpanByte, SpanByte, SpanByteComparer, SpanByteRecordDisposer>>>,
+    BasicContext<byte[], IGarnetObject, ObjectInput, GarnetObjectStoreOutput, long, ObjectSessionFunctions,
+        /* ObjectStoreFunctions */ StoreFunctions<byte[], IGarnetObject, ByteArrayKeyComparer, DefaultRecordDisposer<byte[], IGarnetObject>>,
+        GenericAllocator<byte[], IGarnetObject, StoreFunctions<byte[], IGarnetObject, ByteArrayKeyComparer, DefaultRecordDisposer<byte[], IGarnetObject>>>>>;
+
     internal sealed unsafe partial class ClusterSession : IClusterSession
     {
         long lastLog = 0;
@@ -39,7 +47,7 @@ namespace Garnet.cluster
         /// <param name="invalidParameters"></param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        private bool NetworkClusterMigrate(out bool invalidParameters)
+        private bool NetworkClusterMigrate2(out bool invalidParameters)
         {
             invalidParameters = false;
 
@@ -146,6 +154,126 @@ namespace Garnet.cluster
                 while (!RespWriteUtils.TryWriteDirect(CmdStrings.RESP_OK, ref dcurr, dend))
                     SendAndReset();
             }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Implements CLUSTER MIGRATE command (only for internode use)
+        /// </summary>
+        /// <param name="invalidParameters"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private bool NetworkClusterMigrate(out bool invalidParameters)
+        {
+            invalidParameters = false;
+
+            // Expecting exactly 4 arguments
+            if (parseState.Count != 4)
+            {
+                invalidParameters = true;
+                return true;
+            }
+
+            var replace = parseState.GetArgSliceByRef(1).ReadOnlySpan;
+            var storeType = parseState.GetArgSliceByRef(2).ReadOnlySpan;
+            var payloadStartPtr = parseState.GetArgSliceByRef(3).SpanByte.ToPointer();
+            var lastParam = parseState.GetArgSliceByRef(parseState.Count - 1).SpanByte;
+            var payloadEndPtr = lastParam.ToPointer() + lastParam.Length;
+            var replaceOption = replace.EqualsUpperCaseSpanIgnoringCase("T"u8);
+
+            var storeTypeStr = Encoding.ASCII.GetString(storeType);
+            var buffer = new Span<byte>(payloadStartPtr, (int)(payloadEndPtr - payloadStartPtr)).ToArray();
+            Task.Run(() => Process(basicGarnetApi, buffer, storeTypeStr, replaceOption));
+
+            void Process(BasicGarnetApi basicGarnetApi, byte[] input, string storeTypeSpan, bool replaceOption)
+            {
+                var currentConfig = clusterProvider.clusterManager.CurrentConfig;
+                byte migrateState = 0;
+
+                fixed (byte* ptr = input)
+                {
+                    var payloadPtr = ptr;
+                    var payloadEndPtr = ptr + input.Length;
+                    if (storeTypeSpan.Equals("SSTORE", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var keyCount = *(int*)payloadPtr;
+                        payloadPtr += 4;
+                        var i = 0;
+
+                        TrackImportProgress(keyCount, isMainStore: true, keyCount == 0);
+                        while (i < keyCount)
+                        {
+                            ref var key = ref SpanByte.Reinterpret(payloadPtr);
+                            payloadPtr += key.TotalSize;
+                            ref var value = ref SpanByte.Reinterpret(payloadPtr);
+                            payloadPtr += value.TotalSize;
+
+                            // An error has occurred
+                            if (migrateState > 0)
+                            {
+                                i++;
+                                continue;
+                            }
+
+                            var slot = HashSlotUtils.HashSlot(ref key);
+                            if (!currentConfig.IsImportingSlot(slot)) // Slot is not in importing state
+                            {
+                                migrateState = 1;
+                                i++;
+                                continue;
+                            }
+
+                            // Set if key replace flag is set or key does not exist
+                            var keySlice = new ArgSlice(key.ToPointer(), key.Length);
+                            if (replaceOption || !Exists(ref keySlice))
+                                _ = basicGarnetApi.SET(ref key, ref value);
+                            i++;
+                        }
+                    }
+                    else if (storeTypeSpan.Equals("OSTORE", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var keyCount = *(int*)payloadPtr;
+                        payloadPtr += 4;
+                        var i = 0;
+                        TrackImportProgress(keyCount, isMainStore: false, keyCount == 0);
+                        while (i < keyCount)
+                        {
+                            if (!RespReadUtils.TryReadSerializedData(out var key, out var data, out var expiration, ref payloadPtr, payloadEndPtr))
+                                return;
+
+                            // An error has occurred
+                            if (migrateState > 0)
+                                continue;
+
+                            var slot = HashSlotUtils.HashSlot(key);
+                            if (!currentConfig.IsImportingSlot(slot)) // Slot is not in importing state
+                            {
+                                migrateState = 1;
+                                continue;
+                            }
+
+                            var value = clusterProvider.storeWrapper.GarnetObjectSerializer.Deserialize(data);
+                            value.Expiration = expiration;
+
+                            // Set if key replace flag is set or key does not exist
+                            if (replaceOption || !CheckIfKeyExists(key))
+                                _ = basicGarnetApi.SET(key, value);
+
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("CLUSTER MIGRATE STORE TYPE ERROR!");
+                    }
+                }
+            }
+
+            var currentConfig = clusterProvider.clusterManager.CurrentConfig;
+
+            while (!RespWriteUtils.TryWriteDirect(CmdStrings.RESP_OK, ref dcurr, dend))
+                SendAndReset();
 
             return true;
         }
